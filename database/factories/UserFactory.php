7c9536d95f54209df4698c75a920e6f8bdfadd0d
Phan Viet Tan <tanphan6061@gmail.com>
2021-05-30 01:26:28 +0700

    register page

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
+            // 'email_verified_at' => now(),
             'password' => bcrypt('Demopass69'), // password
             'birthday' => '1999-01-01', // password
             'sex' => $sex[rand(0, count($sex) - 1)], // password
